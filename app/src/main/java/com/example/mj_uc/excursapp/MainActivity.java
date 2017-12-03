896385321e7ff7febfd908dca508cfb705869114
mjucedag <mariajoseuceda@hotmail.com>
2017-12-03 20:31:54 +0100

    JavaDoc Test

--- app/src/main/java/com/example/mj_uc/excursapp/MainActivity.java
+++ app/src/main/java/com/example/mj_uc/excursapp/MainActivity.java
@@ -233,7 +233,7 @@ public class MainActivity extends AppCompatActivity implements Contrato.Vista{
         }
     }
 
-    /*convertir dp a pixel*/
+    /*convert dp a pixel*/
     private int dpToPx(int dp) {
         Resources r = getResources();
         return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
