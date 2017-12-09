9682ac4a1a70a4829f8946184382f9605f5771f8
mjucedag <mariajoseuceda@hotmail.com>
2017-12-09 23:22:40 +0100

    Add JavaDoc in Constants

--- app/src/main/java/com/example/mj_uc/excursapp/tools/Constants.java
+++ app/src/main/java/com/example/mj_uc/excursapp/tools/Constants.java
@@ -1,7 +1,13 @@
 package com.example.mj_uc.excursapp.tools;
 
 
+/**
+ * The type Constants.
+ */
 public class Constants {
 
+    /**
+     * The constant DEFAULT_NAME_IMAGE.
+     */
     public static final String DEFAULT_NAME_IMAGE = "izvicono.png";
 }
